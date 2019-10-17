2c03d1f59c4ba47c4c6c69adb4c1a7734db0ac62
Halo_7777777 <1144265174@qq.com>
2019-10-17 20:41:40 +0800

    :construction:

---
+++ lg/普及/线性数据结构/P1996.cpp
@@ -0,0 +1,27 @@
+//队列方法
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    queue<int> q;
+    int n, m, nowx = 1;
+    scanf("%d%d",&n,&m);
+    for(int i = 1; i <= n; i++)
+        q.push(i);
+    while(!q.empty())
+    {
+        if(nowx == m)
+        {
+            printf("%d ",q.front());
+            q.pop();
+            nowx = 1;
+        }
+        else
+        {
+            q.push(q.front());
+            q.pop();
+            nowx++;
+        }
+    }
+    return 0;
+}
