05dc12cbd2e0a9eb61568fe707b9fd84b6e33c6a
Halo_7777777 <1144265174@qq.com>
2019-10-20 12:29:07 +0800

    practice

--- lg/普及/线性数据结构/P1160.cpp
+++ lg/普及/线性数据结构/P1160.cpp
@@ -1,10 +1,72 @@
-
-
 #include<bits/stdc++.h>
 using namespace std;
-//数组实现的静态双向链表
+//数组实现的静态链表
+//用数组标号代表学生标号
+#define MAXN 100005
+typedef struct node* linklist;
+struct node{
+    int prior;
+    int next;
+};
+int main()
 {
-
+    linklist s = new struct node [MAXN];
+    memset(s,0,sizeof(s));
+    int n, m, k, p, first;
+    scanf("%d",&n);
+    (s+1)->prior = 1;
+    (s+1)->next = 1;
+    first = 1;
+    for(int i = 2; i <= n; i++)
+    {
+        scanf("%d%d",&k,&p);
+        if(p == 1)
+        {
+            s[i].next = s[k].next;
+            s[s[k].next].prior = i;
+            s[k].next = i;
+            s[i].prior = k;
+        }
+        else if(p == 0)
+        {
+            s[s[k].prior].next = i;
+            s[i].prior = s[k].prior;
+            s[i].next = k;
+            s[k].prior = i;
+            if(k == first)
+                first = i;
+        }
+    }
+    int x = 0;
+    scanf("%d",&m);
+    for(int i = 0; i < m; i++)
+    {
+        scanf("%d",&x);
+        if(s[x].next && s[x].prior)  ////记得使用后的指针置0，且不重复删除
+        {
+            s[s[x].prior].next = s[x].next;
+            s[s[x].next].prior = s[x].prior;
+            s[x].next = 0;
+            s[x].prior = 0;
+        }
+        if(x == first)
+        {
+            first = s[x].next;
+        }
+    }
+    for(int i = first, j = 0; ;j++)
+    {
+        if(s[i].next != first)
+            printf("%d ",i);
+        else
+            printf("%d\n",i);
+        i = s[i].next;
+        if(i == first && j != 0)
+        {
+            break;
+        }
+    }
+    return 0;
 }
 //用STL queue会TLE
 /*
