c4fa7b56f3beb48255620d66ac0cdfcf16e2fd24
Halo_7777777 <1144265174@qq.com>
2019-10-22 01:22:17 +0800

    practice

---
+++ lg/普及/深度优先搜索DFS/平方和最大子序列.cpp
@@ -0,0 +1,48 @@
+/*
+给定N个整数（可能有负数），从中选择K个数，
+使得这K个数之和恰好等于一个给定的整数X；
+如果有多种方案，选择它们中元素平方和最大的一个
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define maxn 100001
+//序列A中n个数选k个数使得和为x，最大平方和为maxSumSqu
+int n, k, x, maxSumSqu = -1, A[maxn];
+//temp存放临时方案，ans存放平方和最大的方案
+vector<int> temp, ans;
+//当前处理index号整数， 当前已选整数个数为nowK
+//当前已选整数之和为sum， 当前已选整数平方和为sumSqu
+void DFS(int index, int nowK, int sum, int sumSqu)
+{
+    if(nowK == k && sum == x)
+    {
+        if(sumSqu > maxSumSqu)
+        {
+            maxSumSqu = sumSqu;    //更新最大平方和
+            ans = temp;        //更新最优方案，vector可直接赋值
+        }
+        return;
+    }
+    //已经处理完n个数，或者超过k个数，或者和超过x，返回
+    if(index == n || nowK > k || sum > x) return;
+    temp.push_back(A[index]);   //选index号数
+    DFS(index + 1, nowK + 1, sum + A[index], sumSqu + A[index] * A[index]);
+    temp.pop_back();      //不选index号数
+    DFS(index + 1, nowK, sum, sumSqu);
+}
+int main()
+{
+    scanf("%d%d%d",&n,&k,&x);
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    DFS(0,0,0,0);
+    printf("maxsum = %d\n",maxSumSqu);
+    printf("Squ = ");
+    for(vector<int>::iterator it = ans.begin(); it != ans.end(); it++)
+    {
+        printf("%d ", *it);
+    }
+    return 0;
+}
---
+++ lg/普及/深度优先搜索DFS/背包物品问题.cpp
@@ -0,0 +1,45 @@
+/*
+有n件物品，每件物品的重量为w[i],价值为c[i]。
+现在需要选出若干件物品放入一个容量为V的背包中，
+使得在选入背包的物品重量和不超过容量V的前提下，
+让背包中物品的价值之和最大，求最大价值(1<= n <= 20)
+*/
+
+#include<cstdio>
+const int maxn = 30;
+int n, V, maxValue = 0;   //物品件数n，背包容量V， 最大价值maxValue
+int w[maxn], c[maxn];    //w[i]为每件物品的重量，c[i]为每件物品的价值
+//DFS， index为当前处理的物品编号
+//sumW和sumC分别为当前重量和当前总价值
+void DFS(int index, int sumW, int sumC)
+{
+    if(index == n)    //已经完成对n件物品的选择
+    {
+        return;
+    }
+    DFS(index + 1, sumW, sumC);         //不选第index件物品
+    if(sumW + w[index] <= V)   //只有加入第index件物品后未超过容量V，才能继续
+    {
+        if(sumC + c[index] > maxValue)
+        {
+            maxValue = sumC + c[index];
+        }
+        DFS(index + 1, sumW + w[index], sumC + c[index]);
+    }
+}
+int main()
+{
+    scanf("%d%d", &n, &V);
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &w[i]);     //每件物品的重量
+    }
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &c[i]);     //每件物品的价值
+    }
+    DFS(0, 0, 0);
+    printf("%d\n", maxValue);
+    return 0;
+}
+
